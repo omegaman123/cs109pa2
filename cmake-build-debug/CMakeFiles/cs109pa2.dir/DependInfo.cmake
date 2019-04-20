2c98edcd2e35f94f3846498e18f42f60ea215cf0
Mark <markmdvd@gmail.com>
2019-04-20 13:43:15 -0700

    initial commit.

---
+++ cmake-build-debug/CMakeFiles/cs109pa2.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/markmedved/Desktop/CMPS109/cs109pa2/commands.cpp" "/Users/markmedved/Desktop/CMPS109/cs109pa2/cmake-build-debug/CMakeFiles/cs109pa2.dir/commands.cpp.o"
+  "/Users/markmedved/Desktop/CMPS109/cs109pa2/debug.cpp" "/Users/markmedved/Desktop/CMPS109/cs109pa2/cmake-build-debug/CMakeFiles/cs109pa2.dir/debug.cpp.o"
+  "/Users/markmedved/Desktop/CMPS109/cs109pa2/file_sys.cpp" "/Users/markmedved/Desktop/CMPS109/cs109pa2/cmake-build-debug/CMakeFiles/cs109pa2.dir/file_sys.cpp.o"
+  "/Users/markmedved/Desktop/CMPS109/cs109pa2/main.cpp" "/Users/markmedved/Desktop/CMPS109/cs109pa2/cmake-build-debug/CMakeFiles/cs109pa2.dir/main.cpp.o"
+  "/Users/markmedved/Desktop/CMPS109/cs109pa2/util.cpp" "/Users/markmedved/Desktop/CMPS109/cs109pa2/cmake-build-debug/CMakeFiles/cs109pa2.dir/util.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/cs109pa2.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cs109pa2.dir/commands.cpp.o"
+  "CMakeFiles/cs109pa2.dir/debug.cpp.o"
+  "CMakeFiles/cs109pa2.dir/file_sys.cpp.o"
+  "CMakeFiles/cs109pa2.dir/main.cpp.o"
+  "CMakeFiles/cs109pa2.dir/util.cpp.o"
+  "cs109pa2.pdb"
+  "cs109pa2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cs109pa2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
